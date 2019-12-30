a6749e33e40a47cb72d580ac9c49b2b50b8edda9
kid <kid0510z@163.com>
2019-12-30 23:55:26 +0800

    移除没必要导包

--- src/main/java/com/kid510/vhr/config/SecurityConfig.java
+++ src/main/java/com/kid510/vhr/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kid510.vhr.common.resp.ResultResp;
 import com.kid510.vhr.pojo.Hr;
 import com.kid510.vhr.service.HrService;
-import jdk.nashorn.internal.runtime.ECMAErrors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +11,9 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
