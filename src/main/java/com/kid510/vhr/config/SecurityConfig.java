a70397673dcba43ba4ae0d27d94074fc7f128837
weiWeiSong <songww@viphrm.com>
2020-01-02 09:33:28 +0800

    修改redis配置，使用服务器地址

--- src/main/java/com/kid510/vhr/config/SecurityConfig.java
+++ src/main/java/com/kid510/vhr/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -45,6 +46,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(hrService);
     }
 
+    /**
+     * 忽略
+     *
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/login");
+    }
 
     /**
      * 配置 基本设置
